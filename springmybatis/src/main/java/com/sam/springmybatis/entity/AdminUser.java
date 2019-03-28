b1d1de8a0838ea34c451bf40801766bc8259fbff
yuqiufu <1327499522@qq.com>
2019-03-28 20:53:34 +0800

    修改UserMapper文件,pom.xml文件

--- springmybatis/src/main/java/com/sam/springmybatis/entity/AdminUser.java
+++ springmybatis/src/main/java/com/sam/springmybatis/entity/AdminUser.java
@@ -1,169 +1,44 @@
 package com.sam.springmybatis.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminUser {
 
-	private Integer id;
-	private String username;
-	private String account;
-	private String password;
-	private Date createTime;
-	private Date updateTime;
-	private String email;
-	private String telephone;
-	private Integer age;
-	private String address;
-	private Date birthday;
-	private String handerPic;
-	private Integer isDelete;
-	private Integer male;
-	private Integer forbidden;
+    private Integer id;
 
-	public Integer getId() {
-		return id;
-	}
+    private String username;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    private String account;
 
-	public String getUsername() {
-		return username;
-	}
+    private String password;
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    private Date createTime;
 
-	public String getAccount() {
-		return account;
-	}
+    private Date updateTime;
 
-	public void setAccount(String account) {
-		this.account = account;
-	}
+    private String email;
 
-	public String getPassword() {
-		return password;
-	}
+    private String telephone;
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    private Integer age;
 
-	public Date getCreateTime() {
-		return createTime;
-	}
+    private String address;
 
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
+    private Date birthday;
 
-	public Date getUpdateTime() {
-		return updateTime;
-	}
+    private String handerPic;
 
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
+    private Integer isDelete;
 
-	public String getEmail() {
-		return email;
-	}
+    private Integer male;
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public Date getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-
-	public String getHanderPic() {
-		return handerPic;
-	}
-
-	public void setHanderPic(String handerPic) {
-		this.handerPic = handerPic;
-	}
-
-	public Integer getIsDelete() {
-		return isDelete;
-	}
-
-	public void setIsDelete(Integer isDelete) {
-		this.isDelete = isDelete;
-	}
-
-	public Integer getMale() {
-		return male;
-	}
-
-	public void setMale(Integer male) {
-		this.male = male;
-	}
-
-	public Integer getForbidden() {
-		return forbidden;
-	}
-
-	public void setForbidden(Integer forbidden) {
-		this.forbidden = forbidden;
-	}
-
-	public AdminUser() {
-		super();
-	}
-
-	public AdminUser(Integer id, String username, String account,
-			String password, Date createTime, Date updateTime, String email,
-			String telephone, Integer age, String address, Date birthday,
-			String handerPic, Integer isDelete, Integer male, Integer forbidden) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.account = account;
-		this.password = password;
-		this.createTime = createTime;
-		this.updateTime = updateTime;
-		this.email = email;
-		this.telephone = telephone;
-		this.age = age;
-		this.address = address;
-		this.birthday = birthday;
-		this.handerPic = handerPic;
-		this.isDelete = isDelete;
-		this.male = male;
-		this.forbidden = forbidden;
-	}
+    private Integer forbidden;
 
 }
--- springmybatis/src/main/java/com/sam/springmybatis/entity/User.java
+++ springmybatis/src/main/java/com/sam/springmybatis/entity/User.java
@@ -1,78 +1,28 @@
 package com.sam.springmybatis.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private Integer id;
+
     private String username;
+
     private String age;
+
     private String password;
+
     private String account;
+
     private String address;
+
     private String male;
+
     private String job;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getMale() {
-        return male;
-    }
-
-    public void setMale(String male) {
-        this.male = male;
-    }
-
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
 
 }
